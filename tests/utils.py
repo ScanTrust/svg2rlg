905356e1b6de35d93966b5df0e9ab9eacb242832
Andrew Backer <awbacker@gmail.com>
2017-11-06 17:38:41 +0800

    Update test cases for downloaded files

--- tests/utils.py
+++ tests/utils.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*
 from __future__ import print_function, absolute_import, unicode_literals
 
+import glob
 import os
 import sys
 
 import requests
+from os.path import dirname, realpath
 
-LOCAL_STORAGE = os.path.join(os.path.dirname(os.path.realpath(__file__)), "samples", "download")
+LOCAL_DOWNLOAD_DIR = os.path.join(dirname(realpath(__file__)), "samples", "download")
+MISC_SAMPLES_DIR = os.path.join(dirname(realpath(__file__)), "samples", "misc")
 
 
 def write_line(line):
@@ -39,11 +42,15 @@ def one_liner(text):
     return OneLiner(text)
 
 
+def list_downloaded_files(dest_folder):
+    return glob.glob(os.path.join(LOCAL_DOWNLOAD_DIR, dest_folder, "*.svg"))
+
+
 def fetch_file(server, server_path, dest_folder="misc", dest_file=None):
     """
     Fetch file using http lib module.
     """
-    dest_folder = os.path.join(LOCAL_STORAGE, dest_folder)
+    dest_folder = os.path.join(LOCAL_DOWNLOAD_DIR, dest_folder)
     if not os.path.exists(dest_folder):
         print("Creating dir: %s" % dest_folder)
         os.makedirs(dest_folder)
@@ -55,10 +62,11 @@ def fetch_file(server, server_path, dest_folder="misc", dest_file=None):
         with one_liner("~ downloading %s" % (url,)) as l:
             resp = requests.get(url)
             if resp.status_code == 200:
-                with open(local_file, "wb") as f:
+                with open(local_file, b"wb") as f:
                     f.write(resp.content)
                 l.append(" : success, %sb" % (len(resp.content)))
             else:
                 l.append(" : error => status=%s, text=%s" % (resp.status_code, resp.text.splitlines()[0]))
+        return local_file
 
     return local_file
