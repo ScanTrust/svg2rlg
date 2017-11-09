9270186da0b77b9b6174f4c6c73bc915c491da45
Andrew Backer <awbacker@gmail.com>
2017-11-09 14:24:16 +0800

    Bump version to 1.2.2

--- svg2rlg/__init__.py
+++ svg2rlg/__init__.py
@@ -4,7 +4,7 @@ from __future__ import print_function, absolute_import, unicode_literals
 from svg2rlg.utils import monkeypatch_reportlab
 from .api import data_to_rlg, file_to_rlg
 
-__version__ = "1.2.1"
+__version__ = "1.2.2"
 __license__ = "LGPL 3"
 __author__ = "Dinu Gherman"
 __date__ = "2017-11-08"
